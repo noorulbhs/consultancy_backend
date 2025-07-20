5499cce47416674ac3e0ca21b1a7cf87dace66ad
noorulbhs <noorul.islam658@gmail.com>
2025-07-20 16:05:54 +0530

    fix cors issue

--- src/main/java/com/altrevo/consultancy/config/WebConfig.java
+++ src/main/java/com/altrevo/consultancy/config/WebConfig.java
@@ -38,7 +38,6 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-//                        .allowedOriginPatterns(corsAllowedOrigins)
                         .allowedOrigins(corsAllowedOrigins)
                         .allowedMethods(corsAllowedMethods)
                         .allowedHeaders(corsAllowedHeaders)
