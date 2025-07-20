63b73729bfeaf3f70c661b1b724ccda9ab2284a1
noorulbhs <noorul.islam658@gmail.com>
2025-07-20 13:31:16 +0530

    added * for cors

--- src/main/java/com/altrevo/consultancy/config/WebConfig.java
+++ src/main/java/com/altrevo/consultancy/config/WebConfig.java
@@ -38,7 +38,7 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(corsAllowedOrigins)
+                        .allowedOriginPatterns(corsAllowedOrigins)
                         .allowedMethods(corsAllowedMethods)
                         .allowedHeaders(corsAllowedHeaders)
                         .allowCredentials(corsAllowCredentials);
